e6e4d65f05ea4033be243d8ef1c0b9f66badb0c8
Chris <aromchris@hotmail.com>
2015-03-10 22:01:28 +0000

    ResizeImage by to width/height

---
+++ Resizer/MainForm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resizer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Image image = Image.FromFile("C:\\Users\\Chris\\Desktop\\4.jpg");
+            Bitmap resized = ResizeImage(image,319, 319 );
+            resized.Save("C:\\Users\\Chris\\Desktop\\4resized.jpg", ImageFormat.Jpeg);
+
+        }
+        public Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var resizedImage = new Bitmap(width, height);
+            //Maintains the DPI of the image
+            resizedImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //Anti-Aliasing
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                //Improve rendering quality
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                using (var wrapMode = new ImageAttributes())
+                {
+                    //No ghosting around borders
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            return resizedImage;
+        }
+       
+    }
+}
