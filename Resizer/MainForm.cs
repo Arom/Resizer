ccd13ca72fafb9d2bb69c5c11c21bf2d927a4cd9
Chris <aromchris@hotmail.com>
2015-03-10 22:29:21 +0000

    Resize by %, moved to separate class

--- Resizer/MainForm.cs
+++ Resizer/MainForm.cs
@@ -21,36 +21,14 @@ namespace Resizer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Image image = Image.FromFile("C:\\Users\\Chris\\Desktop\\4.jpg");
-            Bitmap resized = ResizeImage(image,319, 319 );
-            resized.Save("C:\\Users\\Chris\\Desktop\\4resized.jpg", ImageFormat.Jpeg);
-
+            //var resize = new Resize();
+            //Image image = Image.FromFile("C:\\Users\\Chris\\Desktop\\4.jpg");
+            //Bitmap resized = resize.ResizeImage(image, 319, 319);
+            //Bitmap resized = resize.ResizeImagePercent(image, 39);
+            //resized.Save("C:\\Users\\Chris\\Desktop\\4resized.jpg", ImageFormat.Jpeg);
+         
         }
-        public Bitmap ResizeImage(Image image, int width, int height)
-        {
-            var destRect = new Rectangle(0, 0, width, height);
-            var resizedImage = new Bitmap(width, height);
-            //Maintains the DPI of the image
-            resizedImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
-
-            using (var graphics = Graphics.FromImage(resizedImage))
-            {
-                graphics.CompositingMode = CompositingMode.SourceCopy;
-                graphics.CompositingQuality = CompositingQuality.HighQuality;
-                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
-                //Anti-Aliasing
-                graphics.SmoothingMode = SmoothingMode.HighQuality;
-                //Improve rendering quality
-                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
-                using (var wrapMode = new ImageAttributes())
-                {
-                    //No ghosting around borders
-                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
-                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
-                }
-            }
-            return resizedImage;
-        }
+    
        
     }
 }
---
+++ Resizer/Resize.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resizer
+{
+    class Resize
+    {
+        public Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var resizedImage = new Bitmap(width, height);
+            return ProcessImage(resizedImage, image, destRect);
+        }
+        public Bitmap ResizeImagePercent(Image image, int percent)
+        {
+            double resizePercent = (double)percent / 100.00;
+            int width = (int)Math.Round((double)image.Width * resizePercent);
+
+            int height = (int)Math.Round((double)image.Height * resizePercent);
+            var destRect = new Rectangle(0, 0, width, height);
+            var resizedImage = new Bitmap(width, height);
+
+            return ProcessImage(resizedImage, image, destRect);
+        }
+        public Bitmap ProcessImage(Bitmap resizedImage, Image image, Rectangle destRect)
+        {
+            //Maintains the DPI of the image
+
+            resizedImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                //Anti-Aliasing
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                //Improve rendering quality
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                using (var wrapMode = new ImageAttributes())
+                {
+                    //No ghosting around borders
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+                return resizedImage;
+            }
+        }
+    }
+}
